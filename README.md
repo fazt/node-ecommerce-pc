0e99814453d9fb8432ac92d736ddb89b3192eb51
fazttech <fazt@faztweb.com>
2021-01-18 23:00:20 -0500

    render list of products

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+# Laptop Ecommerce
+a simple MPA ecommerce using Javascript stack
+
 # Resources
 * [Rocket Emoji](https://emojipedia.org/rocket/)
+
+# Images
+* [Laptop 1](https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3oYjc?ver=a28c0)
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,6 @@
-export const renderIndex = (req, res) => {
-  res.render("store/index", { title: "Express" });
+import Product from "../models/Product";
+
+export const renderIndex = async (req, res) => {
+  const products = await Product.find();
+  res.render("store/index", { title: "Express", products: products.map(p => p.toJSON()) });
 };
